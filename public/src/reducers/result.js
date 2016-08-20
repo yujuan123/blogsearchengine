9a3415781b2c7f62583d5a9ab18c2493bf86fcde
yujuan <15809216911@163.com>
2016-08-20 09:52:13 +0800

    reducer result

--- public/src/reducers/result.js
+++ public/src/reducers/result.js
@@ -1,10 +1,20 @@
 const result = (state = [], action)=> {
 
   switch (action.type) {
-  case 'TODO_LOADED':
-    return action.data;
-  }
+    case 'ADD_MATERIAL':
+        return [
+            ...state,
+            action.data
+        ];
 
+      break;
+    case 'DELETE_MATERIAL':
+      return state.filter((v,k)=>{
+        return k !== action.id;
+      });
+    case 'INIT':
+        return state;
+  }
   return state;
 };
 
