7d9ac185f6c1a5f11cf46689473d18432d542de4
yujuan <15809216911@163.com>
2016-08-20 09:51:04 +0800

    modify addtodoInput

--- public/src/components/AddTodoInput.js
+++ public/src/components/AddTodoInput.js
@@ -2,11 +2,12 @@ import React, {Component} from 'react'; // eslint-disable-line no-unused-vars
 
 class AddTodoInput extends Component {
 
-  addTodo() {
+  click() {
+    
     let val1 = this.refs.sourceText.value.trim();
     let val2 = this.refs.amountText.value.trim();
     if (val1 && val2) {
-      this.props.addTodo(val1, val2);
+      this.props.addMaterial(val1, val2);
     }
     this.refs.sourceText.value = '';
     this.refs.amountText.value = '';
@@ -16,18 +17,22 @@ class AddTodoInput extends Component {
     if (13 !== e.charCode) {
       return;
     }
-    this.addTodo();
+    this.click();
   }
 
   render() {
     return (
+        
         <form action="#" className="form-horizontal col-sm-8 col-sm-offset-2">
           <div>
-            <label htmlFor="inputEmail3" className="control-label"><h3>用料 <span id="display-message">必填</span></h3></label>
+            <label htmlFor="inputEmail3" className="control-label"><h3>
+              <b>用料</b>
+                <span id="display-message">必填</span>
+            </h3></label>
           </div>
           <div className="form-group">
-            <div className="col-sm-10">
-
+              <div className="col-md-5">
+              用料：
               <input type="text"
                      className="material-input"
                      id="inputSource"
@@ -35,7 +40,9 @@ class AddTodoInput extends Component {
                      ref="sourceText"
                      onKeyPress={this.handleKeyPress.bind(this)}
               />
-
+                </div>
+              <div className="col-md-6">
+              数量：
               <input type="text"
                      className="material-input"
                      id="inputAmount"
@@ -43,9 +50,11 @@ class AddTodoInput extends Component {
                      ref="amountText"
                      onKeyPress={this.handleKeyPress.bind(this)}
               />
-              <button onClick={this.addTodo.bind(this)} type="button" className="btn btn-default btn-sm">添加</button>
+              </div>
+              <button onClick={this.click.bind(this)} 
+                      type="button" id="btn" 
+                      className="btn btn-primary btn-sm">添加</button>
 
-            </div>
           </div>
 
         </form>
