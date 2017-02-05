3e1f5b67bffdbc48c926a5c4989a328190f5341b
yujuan <15809216911@163.com>
2017-02-05 16:47:24 +0800

    Apply the react-router

--- public/src/index.js
+++ public/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import {render} from 'react-dom';
-/*import App from './components/App';*/
-import UserCenter from './components/UserCenter';
+
 import resultList from './reducers';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import {Router, Route, browserHistory,IndexRoute} from 'react-router';
+/*import App from './components/App';*/
+import UserCenter from './components/UserCenter';
+import BlogNewd from './components/BlogNewed';
 
 const store = createStore(
     resultList
@@ -16,7 +19,10 @@ store.dispatch({
 
 render(
     <Provider store={store}>
-      <UserCenter/>
+      <Router history={browserHistory}>
+        <Route path="/" component={UserCenter}/>
+        <Route path="/blogNewed" component={BlogNewd}/>
+      </Router>
     </Provider>,
     document.getElementById('app')
 );
