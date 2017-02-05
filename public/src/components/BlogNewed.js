731cb66c4a7a64acac01c65c2a507effe7d555e1
yujuan <15809216911@163.com>
2017-02-05 23:16:18 +0800

    Add the write content

--- public/src/components/BlogNewed.js
+++ public/src/components/BlogNewed.js
@@ -9,9 +9,21 @@ class BlogNewed extends Component {
         <div>
           <UserCenter/>
           <div className="row">
-            <button type="button" className="btn-info btn-small">My Blogs</button>
+            <button type="button" className="btn-info btn-xs">My Blogs</button>
             <h3>Write,a new blog</h3>
           </div>
+          <form className="row blog-newed">
+            <span className="font-style">Blog Name</span>
+            <span className="iconfont icon-biaoti" > </span>
+            <input type="text" className="form-control blog-name"/>
+            <span className="font-style">Write Here</span>
+            <span className="iconfont icon-write" > </span>
+            <textarea className="form-control" rows="10" cols="10"/>
+            <span className="font-style">Enter Some Tags</span>
+            <span className="iconfont icon-biaoqian" > </span>
+            <input type="text" className="form-control blog-tags"/>
+            <input type="submit" value="Publish This Blog " className="btn-submmit mt20 mb30  btn btn-info "/>
+          </form>
         </div>
     );
   }
