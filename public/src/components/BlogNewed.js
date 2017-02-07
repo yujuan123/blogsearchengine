fbc1edd3eeee69e5424eed523fcb3fc55253612d
yujuan <15809216911@163.com>
2017-02-07 19:58:50 +0800

    Get the value from input

--- public/src/components/BlogNewed.js
+++ public/src/components/BlogNewed.js
@@ -4,27 +4,41 @@
 import React, {Component} from 'react';
 import UserCenter from "./UserCenter";
 class BlogNewed extends Component {
+  handleClick() {
+    var blogName = this.refs.blogName.value.trim();
+    var blogContent = this.refs.blogContent.value.trim();
+    var blogTag = this.refs.blogTag.value.trim();
+    console.log("博客标题 为：" + blogName);
+    console.log("博客内容 为:" + blogContent);
+    console.log("博客标签 为: " + blogTag);
+    /*下一步：发送数据*/
+    /*再下一步：输入框置零*/
+  }
+
   render() {
     return (
-        <div>
-          <UserCenter/>
-          <div className="row">
-            <button type="button" className="btn-info btn-xs">My Blogs</button>
-            <h3>Write,a new blog</h3>
-          </div>
-          <form className="row blog-newed">
-            <span className="font-style">Blog Name</span>
-            <span className="iconfont icon-biaoti" > </span>
-            <input type="text" className="form-control blog-name"/>
-            <span className="font-style">Write Here</span>
-            <span className="iconfont icon-write" > </span>
-            <textarea className="form-control" rows="10" cols="10"/>
-            <span className="font-style">Enter Some Tags</span>
-            <span className="iconfont icon-biaoqian" > </span>
-            <input type="text" className="form-control blog-tags"/>
-            <input type="submit" value="Publish This Blog " className="btn-submmit mt20 mb30  btn btn-info "/>
-          </form>
+      <div>
+        <UserCenter/>
+        <div className="row">
+          <button type="button" className="btn-info btn-xs">My Blogs</button>
+          <h3>Write,a new blog</h3>
         </div>
+        <form className="row blog-newed">
+          <span className="font-style">Blog Name</span>
+          <span className="iconfont icon-biaoti"> </span>
+          <input ref="blogName" type="text" className="form-control blog-name"/>
+          <span className="font-style">Write Here</span>
+          <span className="iconfont icon-write"> </span>
+          <textarea ref="blogContent" className="form-control" rows="10" cols="10"/>
+          <span className="font-style">Enter Some Tags</span>
+          <span className="iconfont icon-biaoqian"> </span>
+          <input ref="blogTag" type="text" className="form-control blog-tags"/>
+          <button type="button" className="btn-submmit mt20 mb30  btn btn-info" onClick={this.handleClick.bind(this)}>
+            <span className="iconfont icon-fabiao"> </span>
+            Publish This Blog
+          </button>
+        </form>
+      </div>
     );
   }
 }
--- server/models/Morning.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-const Morning = mongoose.model('Morning',{
-  val1:String,
-  val2:String
-});
