28cd8be595e7a67237e3767fc4ee52a4d6a2cc51
yujuan <15809216911@163.com>
2017-02-13 19:42:30 +0800

    Add the Link to BlogDetail component

--- public/src/components/BlogItem.js
+++ public/src/components/BlogItem.js
@@ -1,3 +1,20 @@
 /**
  * Created by yujuan on 17-2-11.
  */
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+class BlogItem extends Component {
+  render() {
+    /*获取博客相关信息的数据*/
+    let {id, blogName, blogContent, blogTag, userName, publishDate} = this.props;
+    return (
+        <div>
+          <Link to={"/blogDetail/"+id}>
+            <h2>{blogName}</h2>
+          </Link>
+        </div>
+    )
+  }
+}
+
+export default BlogItem;
