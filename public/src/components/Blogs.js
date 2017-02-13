35046060c914103f12f7ec11d2d59d7a93b78d3c
yujuan <15809216911@163.com>
2017-02-13 19:51:00 +0800

    Get the state from blogMessage reducer

--- public/src/components/Blogs.js
+++ public/src/components/Blogs.js
@@ -1,3 +1,30 @@
-/**
- * Created by yujuan on 17-2-11.
- */
+import React ,{Component} from 'react';
+import {connect} from 'react-redux';
+import BlogItem from './BlogItem';
+class Blogs extends Component{
+  render() {
+    let blogMessage = this.props.blogMessage;
+    return (
+        <div className="container">
+          <div className="row">
+            {
+                blogMessage.map((v,k)=>{
+                  return <BlogItem id={v._id} blogName={v.blogName}
+                  blogContent={v.blogContent} blogTag={v.blogTag}
+                  userName={v.userName} publishDate={v.publishDate}/>
+                })
+            }
+          </div>
+        </div>
+    )
+  }  
+
+}
+/* 返回 由reducer压缩后的 state,并转变（匹配）为props数据，则可以直接this.props.blogMessage */
+const mapStateToProps = (state)=>{
+  return state;
+};
+const BlogsPackage = connect(mapStateToProps)(Blogs);
+export default BlogsPackage;
+  
+  
