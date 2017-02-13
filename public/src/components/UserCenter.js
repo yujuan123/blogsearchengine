c9f3f111a8607099aa34b467c8bd8a7171dc211d
yujuan <15809216911@163.com>
2017-02-13 20:47:37 +0800

    Modify the UserCenter

--- public/src/components/UserCenter.js
+++ public/src/components/UserCenter.js
@@ -5,33 +5,49 @@
 import React, {Component} from 'react';
 /*import react-router中的Link*/
 import {Link} from 'react-router';
-
+import {connect} from 'react-redux';
+import Blogs from './Blogs';
+import {loadUserBlogs} from '../actions/index';
 class UserCenter extends Component {
   render() {
+    this.props.loadUserBlogs();
     return (
-      <div className="row user-info blog-body">
-        <div className="col-md-3 ">
-          <img src="images/userPhoto.jpg" />
+        <div className="container">
+          <div className="row user-info blog-body">
+            <div className="col-md-3 ">
+              <img src="images/userPhoto.jpg"/>
+            </div>
+            <div className=" col-md-9">
+              <ul>
+                <li><h3><span className=" iconfont icon-user-outline"> </span>FanBingBing</h3></li>
+                <li><h3><span className="iconfont  icon-zhiwei"> </span>Actor</h3></li>
+                <li>
+                  <h3>
+                    <Link to="/blogNewed">
+                      <span className="iconfont icon-iconxiezuo01"> </span>
+                      写文章
+                    </Link>
+                  </h3>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="row">
+            <Blogs/>
+          </div>
         </div>
-        <div className=" col-md-4">
-          <ul>
-            <li><h3><span className=" iconfont icon-user-outline"> </span>FanBingBing</h3></li>
-            <li><h3><span className="iconfont  icon-zhiwei"> </span>Actor</h3></li>
-            <li>
-              <h3>
-                <Link to="/blogNewed">
-                  <span className="iconfont icon-iconxiezuo01"> </span>
-                  写文章
-                </Link>
-
-              </h3>
-            </li>
-          </ul>
-        </div>
-      </div>
     );
 
   }
 }
-export default UserCenter;
+const mapStateToProps = (state)=>{
+  return state;
+};
+const mapDispatchToProps = (dispatch)=>({
+  loadUserBlogs:()=>{
+    dispatch(loadUserBlogs());
+  }
+});
+const UserCenterPackage = connect(mapStateToProps,mapDispatchToProps)(UserCenter);
+export default UserCenterPackage;
 
