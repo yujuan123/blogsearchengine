e45d7f31e6b6d1b278d4edcf580fc16fc2e2b37f
yujuan <15809216911@163.com>
2017-02-13 21:03:11 +0800

    Modify the blogDetail reducer

--- public/src/reducers/blogDetail.js
+++ public/src/reducers/blogDetail.js
@@ -1,3 +1,12 @@
 /**
  * Created by yujuan on 17-2-12.
  */
+const blogDetail = (state={},action)=>{
+  switch(action.type){
+    case 'BLOGDETAIL_SHOWED':
+      return action.data;
+  }
+  return state;
+};
+
+export default blogDetail;
