7e44caf23069c8b00723ed5c4a655459a8b26df1
yujuan <15809216911@163.com>
2017-02-13 21:04:51 +0800

    Modify the blogMessage

--- public/src/reducers/blogMessage.js
+++ public/src/reducers/blogMessage.js
@@ -1,3 +1,11 @@
 /**
  * Created by yujuan on 17-2-11.
  */
+const blogMessage = (state=[],action)=>{
+  switch(action.type){
+    case 'BLOGCONTENT_SHOWED':
+      return action.data;
+  }
+  return state;
+};
+export default blogMessage;
