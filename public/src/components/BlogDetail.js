d2874ef492370697acf013dabe915cb5a1c7ea3b
yujuan <15809216911@163.com>
2017-02-16 15:05:23 +0800

    Add the update blog function

--- public/src/components/BlogDetail.js
+++ public/src/components/BlogDetail.js
@@ -4,23 +4,33 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {loadBlogDetail} from '../actions/index';
-/*思路：
- 发送id ,然后根据id 找 到所有数据并显示
- * */
-class BlogDetail extends Component {
+import {sendUpdateBlog} from '../actions/index';
+
+var BlogDetail = React.createClass({
   /*componentDidMount发生在render之前...*/
   componentDidMount() {
     /*为什么要发送id
      * --因为要根据id 找 到对应的博客并显示*/
     this.props.getBlogId(this.props.params.id);
-  }
+  },
+  editClick(){
+    var blogDetailName = document.getElementById("blog-name");
+    var blogName = this.props.blogDetail.blogName;
+    blogDetailName.innerHTML = "<input id='name-input' type='text'  value='" + blogName + "'  />";
+  },
   updateClick(){
-    
-
-  }
+    var blogNameInput = document.getElementById("name-input").value;
+    document.getElementById("blog-name").innerHTML = blogNameInput;
+    this.props.sendUpdateBlog({
+      blogId:this.props.params.id,
+      blogName:blogNameInput
+    });
+    console.log("此篇博客："+this.props.params.id);
+    console.log("博客标题："+blogNameInput);
+  },
   deleteClick(){
     this.props.sendDeleteBlog(this.props.params.id);
-  }
+  },
   render() {
     return (
         <div>
@@ -30,10 +40,16 @@ class BlogDetail extends Component {
                 <button type="button" className="btn-info btn-xs">My Blogs</button>
               </li>
               <li>
-                <button type="button" className="btn-success btn-xs" onClick={this.updateClick.bind(this)}>Update Blogs</button>
+                <button type="button" className="btn-info btn-xs" onClick={this.editClick}>Edit Blogs</button>
               </li>
               <li>
-                <button type="button" className="btn-danger btn-xs" onClick={this.deleteClick.bind(this)}>Delete Blogs</button>
+                <button type="button" className="btn-success btn-xs" onClick={this.updateClick}>Update
+                  Blogs
+                </button>
+              </li>
+              <li>
+                <button type="button" className="btn-danger btn-xs" onClick={this.deleteClick}>Delete Blogs
+                </button>
               </li>
             </ul>
             <h3>Here my blogs</h3>
@@ -41,18 +57,14 @@ class BlogDetail extends Component {
           <form className="row blog-newed">
             <span className="font-style">Blog Name</span>
             <span className="iconfont icon-biaoti"> </span>
-            <input  type="text" className="form-control blog-name" defaultValue={this.props.blogDetail.blogName||""}/>
-            <span className="font-style">Write Here</span>
-            <span className="iconfont icon-write"> </span>
-            <textarea  className="form-control" rows="10" cols="10">{this.props.blogDetail.blogContent}</textarea>
-            <span className="font-style">Enter Some Tags</span>
-            <span className="iconfont icon-biaoqian"> </span>
-            <input  type="text" className="form-control blog-tags" defaultValue={this.props.blogDetail.blogTag||""}/>
+
+            <p id="blog-name">{this.props.blogDetail.blogName} </p>
           </form>
         </div>
     );
   }
-}
+});
+
 const mapStateToProps = (state)=> {
   return state;
 };
@@ -60,14 +72,15 @@ const mapDispatchToProps = (dispatch)=>({
   getBlogId: (id)=> {
     dispatch(loadBlogDetail(id));
   },
-  sendDeleteBlog:(id)=>{
+  sendDeleteBlog: (id)=> {
     dispatch({
-      type:'BLOG_DELETED',
+      type: 'BLOG_DELETED',
       id
     })
+  },
+  sendUpdateBlog:(data)=>{
+    dispatch(sendUpdateBlog(data));
   }
-  
-  
 });
 const BlogDetailPackage = connect(mapStateToProps, mapDispatchToProps)(BlogDetail);
 export default BlogDetailPackage;
